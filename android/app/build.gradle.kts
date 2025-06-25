d5fa5476a0221dd078546893d1f85c842bb74fa9
PruthvirajSinh8080 <jadejapruthviraj8080@gmail.com>
2025-06-25 12:55:55 +0530

    this commit is for migration of project from lenovo laptop to mac pc

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 android {
     namespace = "com.flipcode.career_culture"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "27.0.12077973"
+    ndkVersion = flutter.ndkVersion
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
